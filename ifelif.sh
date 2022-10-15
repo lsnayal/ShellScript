6ee315aaa8685e6375a421809425ce8b29277772
lsnayal <115870586+lsnayal@users.noreply.github.com>
2022-10-15 10:42:16 +0000

    Add files via upload

---
+++ ifelif.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+s=10
+
+if [ $s -lt 10 ]
+then
+  echo number is less than 10
+elif [ $s -eq 10 ]
+   then
+   echo number is equal to 10
+else
+echo number is greater than 10
+fi
+
---
+++ noinput.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+echo $0 $1
+
+if [ $1 -gt 100 ]
+then
+echo $1 is greater than 100
+else
+echo $1 is less than 100
+fi
